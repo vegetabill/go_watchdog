6a0f971da3704789085b7452b21ad0768c7bf7ee
Bill DePhillips <bill.dephillips@gmail.com>
2013-02-10 15:28:22 -0800

    added more test cases around mood and time ago

--- test/go_watchdog_test.rb
+++ test/go_watchdog_test.rb
@@ -6,19 +6,62 @@ require 'last_green_build_fetcher'
 
 class GoWatchdogTest < Test::Unit::TestCase
 
-  def test_knows_if_there_are_any_updates
+  def test_when_fetcher_returns_a_time_knows_if_there_are_any_updates
     with_stub_fetcher_that_returns(Time.now) do
       osito = GoWatchdog.new(simple_config)
       assert osito.any_updates?
     end
   end
 
-  def test_if_fetcher_returns_false_knows_there_are_no_updates
+  def test_when_fetcher_returns_false_knows_there_are_no_updates
     with_stub_fetcher_that_returns(false) do
       osito = GoWatchdog.new(simple_config)
       assert_equal false, osito.any_updates?
     end
   end
+
+  def test_when_fetcher_returns_nil_knows_there_are_no_updates
+    with_stub_fetcher_that_returns(nil) do
+      osito = GoWatchdog.new(simple_config)
+      assert_equal false, osito.any_updates?
+    end
+  end
+
+  def test_time_since_last_green_build
+    with_stub_fetcher_that_returns(Time.now - (60 * 75)) do
+      osito = GoWatchdog.new(simple_config)
+      assert_equal 'about an hour', osito.time_since_last_green_build
+    end
+  end
+
+  def test_mood_when_below_neutral_cutoff_is_happy
+    with_stub_fetcher_that_returns(Time.now - 30) do
+      osito = GoWatchdog.new(simple_config)
+      assert_equal 'happy', osito.mood
+    end
+  end
+
+  def test_mood_when_below_angry_cutoff_is_neutral
+    with_stub_fetcher_that_returns(Time.now - 120) do
+      osito = GoWatchdog.new(simple_config)
+      assert_equal 'neutral', osito.mood
+    end
+  end
+
+  def test_mood_when_below_enraged_cutoff_is_angry
+    with_stub_fetcher_that_returns(Time.now - 330) do
+      osito = GoWatchdog.new(simple_config)
+      assert_equal 'angry', osito.mood
+    end
+  end
+
+  def test_mood_when_above_enraged_cutoff_is_enraged
+    with_stub_fetcher_that_returns(Time.now - 601) do
+      osito = GoWatchdog.new(simple_config)
+      assert_equal 'enraged', osito.mood
+    end
+  end
+
 
   private 
 
@@ -31,11 +74,11 @@ class GoWatchdogTest < Test::Unit::TestCase
         'name' => 'build',
         'stage' => 'units'
       },
-      'mood_cutoff_in_minutes' => 
+      'mood_cutoffs_in_minutes' => 
       {
-        'enraged' => 60,
-        'angry' => 30,
-        'neutral' => 10
+        'enraged' => 10,
+        'angry' => 5,
+        'neutral' => 1
       }
     }
   end
