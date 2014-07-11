77e9a958d43eabf170b0758526eaec20e3a4de91
Bill DePhillips <bill.dephillips@gmail.com>
2014-07-11 12:00:09 -0700

    updated after namespacing helper gem

--- lib/go_watchdog_helper.rb
+++ lib/go_watchdog_helper.rb
@@ -1,18 +1,18 @@
 require 'yaml'
-require 'last_green_build_fetcher'
+require 'go_cd/last_green_build_fetcher'
 
 raise "Must supply Go username in env variable GO_USERNAME" unless ENV['GO_USERNAME']
 raise "Must supply Go password in env variable GO_PASSWORD" unless ENV['GO_PASSWORD']
 
 module GoWatchdogHelper
-  
+
   def config
     @config ||= YAML.load(File.read('config.yml'))
   end
 
   def last_green_build_time
     pipeline_config = config['pipeline']
-    fetcher = LastGreenBuildFetcher.new(:protocol => pipeline_config['protocol'],
+    fetcher = GoCD::LastGreenBuildFetcher.new(:protocol => pipeline_config['protocol'],
                                         :host => pipeline_config['host'],
                                         :port => pipeline_config['port'],
                                         :username => ENV['GO_USERNAME'],
@@ -21,5 +21,5 @@ module GoWatchdogHelper
                                         :stage_name => pipeline_config['stage'])
     fetcher.fetch
   end
-  
+
 end
